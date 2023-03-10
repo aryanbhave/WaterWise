17ac3189e2cab525aa476797cdde7e4f56091e5f
Aryan Bhave <46799871+aryanbhave@users.noreply.github.com>
2023-03-09 16:16:16 -0800

    Update settings.py

--- WaterWise/settings.py
+++ WaterWise/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u%k!$-g2hv45695u$-7^nyrpx_@z=$qr_lmv&cwc%g_mbt_#la
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
