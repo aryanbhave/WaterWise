71eed28c9d0255a21a43f9eb70e75deddb0318f3
phaneendra97 <phaneendra1997@gmail.com>
2023-03-13 17:49:41 -0700

    added model with longer float

--- Logger/models.py
+++ Logger/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class loggerDB(models.Model):
     bottleID=models.CharField(max_length=150)
     timeStamp=models.DateTimeField(auto_now=True, auto_now_add=False)
-    measurement=models.DecimalField(max_digits=5, decimal_places=2)
+    measurement=models.DecimalField(max_digits=15, decimal_places=2)
 
     class Meta:
         db_table='loggerDB'
